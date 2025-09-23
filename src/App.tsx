21e0e9a1f7e2538dc86dee2f1b2e1d3a178adc39
Dhanushka <dhanushkamcr@gmail.com>
2025-09-23 18:58:17 -0500

    use ItemComponent.tsx in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StateCounter from './components/StateCounter';
+import ItemComponent from './components/ItemComponent';
 
 function App() {
   return (
     <div>
       <StateCounter />
+      <ItemComponent />
     </div>  
   );
 }
