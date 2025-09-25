a325c9b4b40291acbc53b10cdff8d921988a094e
Dhanushka <dhanushkamcr@gmail.com>
2025-09-24 19:05:00 -0500

    Implemented custom hook component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StateCounter from './components/StateCounter';
 import ItemComponent from './components/ItemComponent';
+import {useState} from 'react'
+import ThemeContext from './components/ThemeContext';
+import ThemeConsumer from './components/ThemeConsumer';
 
 function App() {
+  const [theme, setTheme] = useState<string>('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
     <div>
       <StateCounter />
       <ItemComponent />
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <ThemeConsumer />
+      </ThemeContext.Provider>
     </div>  
   );
 }
